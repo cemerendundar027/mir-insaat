2472bf5e3effc12346f110c8a58b26ee5e31f9fe
cemerendundar027 <cemerendundar027@gmail.com>
2026-05-01 15:47:44 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/-nsaat-Sirketi/",
+  base: "/mir-insaat/",
   plugins: [react()],
 });
